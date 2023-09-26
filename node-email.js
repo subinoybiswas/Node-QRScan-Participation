4920478e459ed01750116ea345d9d0b8cf0d76b1
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-09-26 23:00:00 +0530

    Not initial

---
+++ node-email.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware to parse URL-encoded and JSON request bodies
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/data", async (req, res) => {
+  // Extract query parameters from the URL
+  const name = req.query.name;
+  const email = req.query.email;
+  const phone = req.query.phone;
+
+  // Replace with your Google Apps Script Web App URL
+  const appsScriptUrl =
+    process.env.URL;
+
+  try {
+    // Send an HTTP POST request to your Google Apps Script with the extracted data
+    const response = await axios.post(appsScriptUrl, { name, email, phone });
+    res
+      .status(200)
+      .send("POST request sent successfully to Google Apps Script.");
+  } catch (error) {
+    console.error("Error sending POST request:", error);
+    res.status(500).send("Error sending POST request to Google Apps Script.");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
